af6f187664563561732e42840677dd594b09fb34
Jean-François ORNECH <jean-francois.ornech@ac-poitiers.fr>
2025-09-28 22:48:26 +0200

    fix appel fonction sh

--- infrastructure/create_lab.sh
+++ infrastructure/create_lab.sh
@@ -90,12 +90,12 @@ fix_perms_data() {
 }
 
 # -------- Run workflow --------
-ensure_lab_network "$LAB_NAME"
-generate_certs "$LAB_NAME" "$CERTS_DIR"
 generate_env "$LAB_NAME" "$LAB_DIR"
 generate_fileossec "$LAB_NAME" "$CONFIG_DIR/ossec"
 generate_compose "$LAB_NAME" "$COMPOSE_TEMPLATE" "$COMPOSE_FILE"
 generate_nginx_conf "$LAB_NAME" "$NGINX_CONF_DIR"
+generate_certs "$LAB_NAME" "$CERTS_DIR"
+
 
 fix_perms_certs
 fix_perms_data
