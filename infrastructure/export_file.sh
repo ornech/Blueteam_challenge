75b8ad4c4da33ee691e53940eaad2508632d36e0
Jean-François ORNECH <jean-francois.ornech@ac-poitiers.fr>
2025-10-02 10:45:52 +0200

    Fic manager
    DISABLE_FILEBEAT=1

---
+++ infrastructure/export_file.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cat create_lab.sh
+echo "========================================"
+cat lib/generate.sh
+echo "========================================"
+cat docker-compose-template.yml
